e994b698216238e0f3c17dc2b1d9359f5cc8336a
Yorfad <127054635+Yorfad@users.noreply.github.com>
2023-09-11 02:15:45 -0600

    click Proyectos

--- javaindex.js
+++ javaindex.js
@@ -1,1 +1,9 @@
+const tarjetaProyectos = document.querySelector(".contenedor--tarjetasProyectos");
+
+tarjetaProyectos.addEventListener("click", e =>{
+
+    const url = "carritoDeCompras/carrito.html";
+
+    window.location.href = url;
+} )
 
